75f8a6f98d0718624c891a1496394a6e4685676f
Benjamin Stone <benjaminstone@Benjamins-MacBook-Pro-2.local>
2020-04-17 17:10:59 -0400

    add neighboring keys work

--- SubstitutionCipherDecoder/SubstitutionCiperDecoder.swift
+++ SubstitutionCipherDecoder/SubstitutionCiperDecoder.swift
@@ -1,11 +1,29 @@
 import Foundation
 
+struct NGramUtility {
+    static let bigramLetterProbability = NGramDistance(frombundleFileWithName: "count-2l.txt")!
+    static let trigramLetterProbability = NGramDistance(frombundleFileWithName: "count-3l.txt")!
+    static func letterNGrams(n: Int, from words: [String]) -> [String] {
+        var nGrams = [String]()
+        for word in words {
+            let wordArr = Array(word).map { String($0) }
+            for i in 0..<wordArr.count {
+                nGrams.append(wordArr[i..<i+n].reduce("", +))
+            }
+        }
+        return nGrams
+    }
+    static func letterNGrams(n: Int, from msg: String) -> [String] {
+        // TODO: Add support for splitting string into probable words
+        fatalError()
+    }
+}
+
 struct SubstitutionCiperDecoder {
     var dict: [Character: Character] = [:]
-    private let bigramLetterProbability = NGramDistance(frombundleFileWithName: "count-2l.txt")!
-    private let trigramLetterProbability = NGramDistance(frombundleFileWithName: "count-3l.txt")!
+    
     func decode(words: [String]) -> String {
-        let trigrams = letterNGrams(n: 3, from: words)
+        let trigrams = NGramUtility.letterNGrams(n: 3, from: words)
         return ""
     }
     private func localMaximum(from words: [String],
@@ -23,25 +41,43 @@ struct SubstitutionCiperDecoder {
     }
     
     private func evaluatePosition(from trigrams: [String]) -> Double {
-        return trigrams.map { trigramLetterProbability.getFrequency(forKey: $0) }.reduce(0) { $0 + log10($1) }
+        return trigrams.map { NGramUtility.trigramLetterProbability.getFrequency(forKey: $0) }.reduce(0) { $0 + log10($1) }
     }
     
     private func neighboringKeys(from words: [String], using key: [Character: Character]) {
         
     }
     
-    private func letterNGrams(n: Int, from words: [String]) -> [String] {
-        var nGrams = [String]()
-        for word in words {
-            let wordArr = Array(word).map { String($0) }
-            for i in 0..<wordArr.count {
-                nGrams.append(wordArr[i..<i+n].reduce("", +))
-            }
+    struct NeighboringKeys: Sequence, IteratorProtocol {
+        let key: [Character: Character]
+        let decryptedMessage: [String]
+        
+        init(key: [Character: Character], decryptedMessage: [String]) {
+            self.key = key
+            self.decryptedMessage = decryptedMessage
         }
-        return nGrams
-    }
-    private func letterNGrams(n: Int, from msg: String) -> [String] {
-        // TODO: Add support for splitting string into probable words
-        fatalError()
+        mutating func next() -> [Character: Character]? {
+            let bigramFrequencies = NGramUtility.bigramLetterProbability.frequencies
+            let bigrams = NGramUtility.letterNGrams(n: 2, from: decryptedMessage).sorted { nGram1, nGram2 in
+                return bigramFrequencies[nGram1] ?? Double.greatestFiniteMagnitude < bigramFrequencies[nGram2] ?? Double.greatestFiniteMagnitude
+                }[0..<30]
+            for bigram in bigrams {
+                let firstLetter = bigrams.first!
+                let secondLetter = bigrams.last!
+                let shuffledAlphabet = Array("abcdefghijklmnopqrstuvwxyz").map { "\($0)" }.shuffled()
+                for alphaLetter in shuffledAlphabet {
+                    if firstLetter == secondLetter && bigramFrequencies[alphaLetter + alphaLetter]! > bigramFrequencies[firstLetter + secondLetter] {
+                        return
+                    }
+                }
+            }
+            
+            return nil
+        }
+        private func keyswap(key: [Character: Character], firstLetter: String, secondLetter: String) {
+            var newKey = key
+            newKey[firstLetter + secondLetter] = secondLetter + firstLetter
+        }
     }
 }
+
